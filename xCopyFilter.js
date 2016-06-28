80782161caf0d58d989955ba833a4e70952f0b71
钟颖 <cyan.zy@alibaba-inc.com>
2016-06-28 19:31:05 +0800

    Test

--- xCopyFilter.js
+++ xCopyFilter.js
@@ -1,3 +1,11 @@
-document.addEventListener('copy', function(e) {
-	alert("copy: " + window.getSelection());
-}); 
+document.addEventListener('copy', function(event) {
+	event.stopPropagation();
+	event.preventDefault();
+	var node = document.createElement('div');
+	var selection = window.getSelection();
+	node.appendChild(selection.getRangeAt(0).cloneContents());
+	var htmlData = node.innerHTML;
+	var textData = selection.getRangeAt(0);
+	event.clipboardData.setData("text/html", htmlData);
+	event.clipboardData.setData("text/plain", textData);	
+});
